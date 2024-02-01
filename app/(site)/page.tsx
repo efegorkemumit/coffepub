9eb0bf52aef9253ca041492cf766fc09a9d95164
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-01 06:48:25 +0300

    CoffePub1

---
+++ app/(site)/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
+import { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+
+
+export default function Home() {
+
+  useEffect(()=>{
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+  }, []);
+  return (
+    <>
+    <Navbar/>
+    
+    </>
+  );
+}
